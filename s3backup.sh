f0ac9e213167367f8ac8f65dccd621387be00a35
sourdurian <57052760+sourdurian@users.noreply.github.com>
2021-09-10 20:44:30 +0800

    Update s3backup.sh

--- s3backup.sh
+++ s3backup.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-export PGHOST=localhost
-export PGPORT=5432
-export PGDATABASE=suppliers
-export PGUSER=abc
-export PGPASSWORD=*put your own pw*
+export PG_DB_HOST=localhost
+export PG_DB_PORT=5432
+export PG_DB_NAME=suppliers
+export PG_DB_USER=abc
+export PG_DB_PW=<use your own pw>
 
 TIME=$(date "+%s")
-BACKUP_FILE="postgres_${PGDATABASE}_${TIME}.pgdump"
+BACKUP_FILE="postgres_${PG_DB_NAME}_${TIME}.pgdump"
+echo "Backing up $PG_DB_NAME to $BACKUP_FILE"
 pg_dump --format=custom > $BACKUP_FILE
 echo "Backup completed"
 
-S3_BUCKET=s3://path/to/bucket
+S3_BUCKET=s3://ai-model-database-backup/postgresql
 S3_TARGET=$S3_BUCKET/$BACKUP_FILE
 echo "Copying $BACKUP_FILE to $S3_TARGET"
 aws s3 cp $BACKUP_FILE $S3_TARGET
 
-echo "Backup completed for $PGDATABASE"
+echo "Backup completed for $PG_DB_NAME"
 
-BACKUP_RESULT=$(aws s3 ls $S3_BUCKET | tail -n 1)
-echo "Latest S3 backup: $BACKUP_RESULT"
 
